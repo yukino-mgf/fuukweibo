56b7290e7864b88925df1474225e7227650b4a40
emt-mgf <emilia.m.t@qq.com>
2021-12-11 23:36:31 +0800

    伪时序伪协同过滤

---
+++ src/sort.py
@@ -0,0 +1,13 @@
+import os
+
+
+def generate_sortedfile(input_file_path, out_file_path):
+    inputfile = open(input_file_path,encoding='utf-8')
+
+    entrys = inputfile.readlines()
+    entrys.sort(key=lambda x: x.split('\t')[2].split(' ')[0])
+    sortedfile = open(out_file_path, "w",encoding='utf-8')
+    for i in entrys:
+        sortedfile.write(i)
+    sortedfile.close()
+    inputfile.close()
---
+++ src/split.py
@@ -0,0 +1,22 @@
+import os
+from datetime import *
+delimiter = "\t"
+
+def train_date_split(weibo_train_file_path, seperate_day, begin_date,weibo_train_five_file_path,weibo_train_last_file_path):
+    weibo_train_file = open(weibo_train_file_path,encoding= 'utf-8')     
+    ffive = open(weibo_train_five_file_path,'w',encoding= 'utf-8') 
+    flast = open(weibo_train_last_file_path,'w',encoding= 'utf-8') 
+    interval_days = (seperate_day-begin_date).days
+    for line in weibo_train_file:
+        uid, mid, time, forward_count,comment_count, like_count, content = line.split(delimiter)         
+        real_date=date(*parse_date(time))
+        date_delta = (real_date - begin_date).days
+        if interval_days<date_delta:
+            flast.write(line)
+        else:
+            ffive.write(line)     
+
+def parse_date(raw_date):
+    entry_date = raw_date
+    year, month, day = entry_date.split(" ")[0].split("-")
+    return int(year), int(month), int(day)
